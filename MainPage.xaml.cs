31d0c04bdf43b098120f4736816092886c4a1178
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-09-23 17:12:50 +0200

    Dodaj pliki projektów.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace DomekWGorach
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        int state = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            if(pressed=="Zapisz")
+                state = 1;
+            if(pressed== "Polub"&&state==0)
+                count++;
+            if (pressed == "Usuń" && count>0 && state == 0)
+                count--;
+            this.licznik.Text = $"{count} polubień";
+
+
+            SemanticScreenReader.Announce(licznik.Text);
+        }
+    }
+
+}
